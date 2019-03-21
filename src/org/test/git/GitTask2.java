e8190b16a3b5b651f87d794b46b3482acd7ca949
PriyaMani@123 <bpriyamca07@gmail.com>
2019-03-21 11:25:19 +0530

    Addd

--- src/org/test/git/GitTask2.java
+++ src/org/test/git/GitTask2.java
@@ -1,9 +1,19 @@
 package org.test.git;
 
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
 public class GitTask2 {
+	
+
+	public void add()
+	{
+		int c=10+20;
+		System.out.println(c);
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("git2");
+		
 	}
 }
